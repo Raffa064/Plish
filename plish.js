d109c76cc9632547f31f44254aa7e98fa85e2b5d
Raffa064 <raffaddtg@gmail.com>
2023-12-06 23:44:19 -0300

    Fixed example

--- plish.js
+++ plish.js
@@ -4,13 +4,5 @@ const Compiler = require("./compiler/Compiler.js");
 const compiler = new Compiler();
 const source = fs.readFileSync("./output.bin" /*example.plish"*/, "utf-8");
 
-const bin = [];
-
-for (let i = 0; i < source.length; i++) {
-  bin.push(source.charCodeAt(i));
-}
-
-console.log(bin);
-
 //console.log(require("./compiler/Tokenizer.js")().tokenize(source));
-//console.log(compiler.compile(source, "output.bin"));
+console.log(compiler.compile(source, "output.bin"));
